cbbfbe2e03582c0ab4f25b407af9ff3a7d18f116
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-07-20 06:08:52 -0400

    learning digital ledgers by building blockchain

---
+++ learn-digital-ledgers-by-building-a-blockchain/blockchain-helpers.js
@@ -0,0 +1,13 @@
+import { writeFileSync, readFileSync } from 'fs';
+
+export function writeBlockchain(blockchain) {
+  const blockchainString = JSON.stringify(blockchain, null, 2)
+
+  writeFileSync('./blockchain.json', blockchainString);
+}
+
+export function getBlockchain() {
+  const blockchainFile = readFileSync('./blockchain.json')
+  const blockchain = JSON.parse(blockchainFile)
+  return blockchain
+}
